4aa07bbc3900b6f5e8538087768fa21b61add8b8
Umer Edava <umeredava@gmail.com>
2021-05-04 08:51:24 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import Logo from './assets/Stargram icon.jpg'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+          source={{
+            uri: 'https://media-exp1.licdn.com/dms/image/C560BAQEWWGiLkZLOKQ/company-logo_200_200/0/1607925908164?e=2159024400&v=beta&t=fLPSTq0uYhQy-Pm1Sg0SdeidTs78JLqBy9eU1vI1wvo',
+          }}
+          style={{ width: 60, height: 60 }}
+        />
+
+        {/* <TextInput
+              style={{
+                height: 40,
+                width: 150, 
+                borderColor: 'gray',
+                borderWidth: 1
+              }}
+           
+              placeholder="Search"
+        /> */}
+
+      <Text style={styles.heading}>STARGRAM</Text>
+
       <StatusBar style="auto" />
     </View>
-  );
+  );     
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+      color: 'yellow',  
+      position: 'absolute',
+      bottom: '10px'   
+  } 
 });
